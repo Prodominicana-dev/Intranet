d47e608bcb53f889ff24d712cb99ee6e927b63ff
RandyFrias <randy140801@gmail.com>
2025-03-31 16:28:23 -0400

    test /admin

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "**",
-          },
-          {
-            protocol: "http",
-            hostname: "localhost",
-            pathname: "**",
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
       },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        pathname: "**",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/admin",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
